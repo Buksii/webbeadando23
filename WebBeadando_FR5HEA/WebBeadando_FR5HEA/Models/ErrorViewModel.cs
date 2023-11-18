37f84d5e7b2f722fe7ae40ae736b13c02e4d7b59
Buksii <bencecsengeri@gmail.com>
2023-11-18 22:26:31 +0100

    Base

---
+++ WebBeadando_FR5HEA/WebBeadando_FR5HEA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBeadando_FR5HEA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
